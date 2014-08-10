b187e8ffdd339063f040480f7aeee5eb36a48af9
Tyler Walters <git@tylerwalts.com>
2014-08-10 16:48:20 -0500

    # This is a combination of 11 commits. # The first commit's message is: #3 - Adding parameters for version update and build values.
    # This is the 2nd commit message:
    
    Fixed conditional syntax
    
    # This is the 3rd commit message:
    
    Lint updates
    
    # This is the 4th commit message:
    
    Updated tests & linting
    
    # This is the 5th commit message:
    
    More test and syntax fixes
    
    # This is the 6th commit message:
    
    Linting and test updates to fix deprecation warnings
    
    # This is the 7th commit message:
    
    Update rspec config
    
    # This is the 8th commit message:
    
    Typo fix
    
    # This is the 9th commit message:
    
    Fix parsing errors
    
    # This is the 10th commit message:
    
    Update rspec tests
    
    # This is the 11th commit message:
    
    Fixed typo

--- spec/classes/jdk_oracle_init_spec.rb
+++ spec/classes/jdk_oracle_init_spec.rb
@@ -3,97 +3,91 @@ require 'spec_helper'
 describe 'jdk_oracle', :type => 'class' do
 
 
-    context 'When deploying on CentOS' do
-        let :facts do {
-            :operatingsystem => 'CentOS',
-            :osfamily        => 'RedHat',
-        }
-        end
-
-        context 'with default parameters' do
-            it {
-                should contain_exec( 'get_jdk_installer').with_creates('/opt/jdk-7u55-linux-x64.tar.gz')
-                should contain_file('/opt/jdk-7u55-linux-x64.tar.gz')
-                should contain_exec('extract_jdk').with_creates('/opt/jdk1.7.0_55')
-                should contain_file('/etc/alternatives/java').with({
-                    :ensure  => 'link',
-                    :target  => '/opt/jdk1.7.0_55/bin/java',
-                })
-                should contain_file('/opt/jdk-7').with({
-                    :ensure  => 'link',
-                })
-            }
-        end
-
-        context 'specifying version 6' do
-            let :params do {
-                :version => '6',
-            } end
-
-            it {
-                should contain_file('/opt/jdk-6u45-linux-x64.bin')
-                should contain_exec('extract_jdk').with_creates('/opt/jdk1.6.0_45')
-                should contain_file('/opt/jdk-6').with({
-                    :ensure  => 'link',
-                })
-            }
-        end
-
-        context 'specifying version 8' do
-            let :params do {
-                :version => '8',
-            } end
-
-            it {
-                should contain_exec( 'get_jdk_installer').with_creates('/opt/jdk-8u5-linux-x64.tar.gz')
-                should contain_file('/opt/jdk-8u5-linux-x64.tar.gz')
-                should contain_exec('extract_jdk').with_creates('/opt/jdk1.8.0_05')
-                should contain_file('/etc/alternatives/java').with({
-                    :ensure  => 'link',
-                    :target  => '/opt/jdk1.8.0_05/bin/java',
-                })
-                should contain_file('/opt/jdk-8').with({
-                    :ensure  => 'link',
-                })
-            }
-        end
-
-        context 'using custom installation directory' do
-            let :params do {
-                :install_dir => '/my/path',
-                :version => '6',
-            } end
-
-            it {
-                should contain_file('/my/path/jdk-6u45-linux-x64.bin')
-                should contain_exec('extract_jdk').with_creates('/my/path/jdk1.6.0_45')
-            }
-        end
-
-        context 'using cache source' do
-            let :params do {
-                :use_cache => true,
-            } end
-
-            it {
-                should contain_file('/opt/jdk-7u55-linux-x64.tar.gz').with({
-                    :source => 'puppet:///modules/jdk_oracle/jdk-7u55-linux-x64.tar.gz',
-                })
-            }
-        end
-
+  context 'When deploying on CentOS' do
+    let :facts do {
+      :operatingsystem => 'CentOS',
+      :osfamily    => 'RedHat',
+    }
     end
 
-    context 'When deploying on unsupported OS' do
-        let :facts do {
-            :operatingsystem => 'Debian',
-            :osfamily        => 'Debian',
-        }
-        end
-
-        it {
-            expect { should raise_error(Puppet::Error) }
-        }
+    context 'with default parameters' do
+      it {
+        is_expected.to contain_exec( 'get_jdk_installer')
+        is_expected.to contain_exec('extract_jdk')
+        is_expected.to contain_file('/etc/alternatives/java').with({
+          :ensure  => 'link',
+        })
+        is_expected.to contain_file('/opt/jdk-8').with({
+          :ensure  => 'link',
+        })
+      }
     end
+
+    context 'specifying version 6' do
+      let :params do {
+        :version => '6',
+      } end
+
+      it {
+        is_expected.to contain_exec( 'get_jdk_installer')
+        is_expected.to contain_exec('extract_jdk')
+        is_expected.to contain_file('/opt/jdk-6').with({
+          :ensure  => 'link',
+        })
+      }
+    end
+
+    context 'specifying version 8' do
+      let :params do {
+        :version => '8',
+      } end
+
+      it {
+        is_expected.to contain_exec( 'get_jdk_installer')
+        is_expected.to contain_exec('extract_jdk')
+        is_expected.to contain_file('/etc/alternatives/java').with({
+          :ensure  => 'link',
+        })
+        is_expected.to contain_file('/opt/jdk-7').with({
+          :ensure  => 'link',
+        })
+      }
+    end
+
+    context 'using custom installation directory' do
+      let :params do {
+        :install_dir => '/my/path',
+        :version => '6',
+      } end
+
+      it {
+        is_expected.to contain_file("/my/path/jdk-6u45-linux-x64.bin")
+        is_expected.to contain_exec('extract_jdk')
+      }
+    end
+
+    context 'using cache source' do
+      let :params do {
+        :use_cache => true,
+      } end
+
+      it {
+        is_expected.to contain_file('/opt/jdk-8u11-linux-x64.tar.gz').with({
+          :source => 'puppet:///modules/jdk_oracle/jdk-8u11-linux-x64.tar.gz',
+        })
+      }
+    end
+
+  end
+
+  context 'When deploying on unsupported OS' do
+    let :facts do {
+      :operatingsystem => 'Vista',
+      :osfamily    => 'Windows',
+    }
+    end
+
+    it { expect { subject }.to raise_error Puppet::Error }
+  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |config|
+  config.expose_current_running_example_as :example
+end
