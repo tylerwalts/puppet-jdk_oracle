127af0dca0b16b3d9bf424a66987f12ec28b63b1
Hosh Sadiq <hosh.sadiq@constant.co>
2016-06-21 22:42:58 +0100

    Add support for JRE and Server JRE

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 ## Overview
 
-Puppet module to automate fetching and installing the Oracle JDK from the Oracle-hosted download site located here: http://www.oracle.com/technetwork/java/javase/downloads/index.html
+Puppet module to automate fetching and installing the Oracle JDK/JRE/Server-JRE from the Oracle-hosted download site located here: http://www.oracle.com/technetwork/java/javase/downloads/index.html
 
 _Note:  By using this module you will automatically accept the Oracle agreement to download Java._
 
-There are several puppet modules available that will help install Oracle JDK, but they either use the local package manager repository, or depend on the user to manually place the Oracle Java installer in the module's file directory prior to using.  This module will use wget with a cookie to automatically grab the installer from Oracle.
+There are several puppet modules available that will help install Oracle JDK/JRE/Server-JRE, but they either use the local package manager repository, or depend on the user to manually place the Oracle Java installer in the module's file directory prior to using.  This module will use wget with a cookie to automatically grab the installer from Oracle.
 
 This approach was inspired by: http://stackoverflow.com/questions/10268583/how-to-automate-download-and-instalation-of-java-jdk-on-linux
 
@@ -105,6 +105,8 @@ site.pp:
     *  Optionally host the installer file locally instead of fetching it each time, for faster dev & test
 *  platform
     *  The platform to use
+*  package
+    *  The package to install. Can be one of the following: jdk, jre, server-jre
 *  jce
     * Boolean to optionally install Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files (Java 8 only)
 *  default_java
--- manifests/install.pp
+++ manifests/install.pp
@@ -6,6 +6,7 @@ define jdk_oracle::install(
   $use_cache      = false,
   $cache_source   = 'puppet:///modules/jdk_oracle/',
   $platform       = 'x64',
+  $package        = 'jdk',
   $jce            = false,
   $default_java   = true,
   $create_symlink = true,
@@ -29,6 +30,14 @@ define jdk_oracle::install(
       default: { fail("Unsupported platform: ${platform}.  Implement me?") }
     }
 
+    if $package != 'jdk' and $package != 'server-jre' and $package != 'jre' {
+      fail("Unsupported package: ${package}.  Implement me?")
+    }
+
+    $package_home = $package ? {
+      'jre' => 'jre',
+      default => 'jdk'
+    }
     case $version {
       '8': {
         if ($version_update != 'default') {
@@ -41,8 +50,8 @@ define jdk_oracle::install(
         } else {
           $version_b = $default_8_build
         }
-        $javaDownloadURI = "http://download.oracle.com/otn-pub/java/jdk/${version}u${version_u}-b${version_b}/jdk-${version}u${version_u}-linux-${plat_filename}.tar.gz"
-        $java_home = "${install_dir}/jdk1.${version}.0_${version_u}"
+        $javaDownloadURI = "http://download.oracle.com/otn-pub/java/jdk/${version}u${version_u}-b${version_b}/${package}-${version}u${version_u}-linux-${plat_filename}.tar.gz"
+        $java_home = "${install_dir}/${package_home}1.${version}.0_${version_u}"
         $jceDownloadURI = 'http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip'
       }
       '7': {
@@ -56,8 +65,8 @@ define jdk_oracle::install(
         } else {
           $version_b = $default_7_build
         }
-        $javaDownloadURI = "http://download.oracle.com/otn-pub/java/jdk/${version}u${version_u}-b${version_b}/jdk-${version}u${version_u}-linux-${plat_filename}.tar.gz"
-        $java_home = "${install_dir}/jdk1.${version}.0_${version_u}"
+        $javaDownloadURI = "http://download.oracle.com/otn-pub/java/jdk/${version}u${version_u}-b${version_b}/${package}-${version}u${version_u}-linux-${plat_filename}.tar.gz"
+        $java_home = "${install_dir}/${package_home}1.${version}.0_${version_u}"
       }
       '6': {
         if ($version_update != 'default'){
@@ -70,8 +79,8 @@ define jdk_oracle::install(
         } else {
           $version_b = $default_6_build
         }
-        $javaDownloadURI = "https://edelivery.oracle.com/otn-pub/java/jdk/${version}u${version_u}-b${version_b}/jdk-${version}u${version_u}-linux-${plat_filename}.bin"
-        $java_home = "${install_dir}/jdk1.${version}.0_${version_u}"
+        $javaDownloadURI = "https://edelivery.oracle.com/otn-pub/java/jdk/${version}u${version_u}-b${version_b}/${package}-${version}u${version_u}-linux-${plat_filename}.bin"
+        $java_home = "${install_dir}/${package_home}1.${version}.0_${version_u}"
       }
       default: {
         fail("Unsupported version: ${version}.  Implement me?")
@@ -91,13 +100,13 @@ define jdk_oracle::install(
         source  => "${cache_source}${installerFilename}",
         require => File[$install_dir],
       } ->
-      exec { "get_jdk_installer_${version}":
+      exec { "get_${package}_installer_${version}":
         cwd     => $install_dir,
-        creates => "${install_dir}/jdk_from_cache",
-        command => 'touch jdk_from_cache',
+        creates => "${install_dir}/${package}_from_cache",
+        command => 'touch ${package}_from_cache',
       }
     } else {
-      exec { "get_jdk_installer_${version}":
+      exec { "get_${package}_installer_${version}":
         cwd     => $install_dir,
         creates => "${install_dir}/${installerFilename}",
         command => "wget -c --no-cookies --no-check-certificate --header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com\" --header \"Cookie: oraclelicense=accept-securebackup-cookie\" \"${javaDownloadURI}\" -O ${installerFilename}",
@@ -107,7 +116,7 @@ define jdk_oracle::install(
 
       file { "${install_dir}/${installerFilename}":
         mode    => '0755',
-        require => Exec["get_jdk_installer_${version}"],
+        require => Exec["get_${package}_installer_${version}"],
       }
 
       if ! defined(Package['wget']) {
@@ -119,16 +128,16 @@ define jdk_oracle::install(
 
     # Java 7/8 comes in a tarball so just extract it.
     if ( $version in [ '7', '8' ] ) {
-      exec { "extract_jdk_${version}":
+      exec { "extract_${package}_${version}":
         cwd     => "${install_dir}/",
         command => "tar -xf ${installerFilename}",
         creates => $java_home,
-        require => Exec["get_jdk_installer_${version}"],
+        require => Exec["get_${package}_installer_${version}"],
       }
     }
     # Java 6 comes as a self-extracting binary
     if ( $version == '6' ) {
-      exec { "extract_jdk_${version}":
+      exec { "extract_${package}_${version}":
         cwd     => "${install_dir}/",
         command => "${install_dir}/${installerFilename}",
         creates => $java_home,
@@ -141,7 +150,7 @@ define jdk_oracle::install(
       recurse   => true,
       owner     => root,
       group     => root,
-      subscribe => Exec["extract_jdk_${version}"],
+      subscribe => Exec["extract_${package}_${version}"],
     }
 
     # Set links depending on osfamily or operating system fact
@@ -151,27 +160,31 @@ define jdk_oracle::install(
           file { '/etc/alternatives/java':
             ensure  => link,
             target  => "${java_home}/bin/java",
-            require => Exec["extract_jdk_${version}"],
+            require => Exec["extract_${package}_${version}"],
           }
-          file { '/etc/alternatives/javac':
-            ensure  => link,
-            target  => "${java_home}/bin/javac",
-            require => Exec["extract_jdk_${version}"],
+          if $package != 'jre' {
+            file { '/etc/alternatives/javac':
+              ensure  => link,
+              target  => "${java_home}/bin/javac",
+              require => Exec["extract_${package}_${version}"],
+            }
           }
           file { '/etc/alternatives/jar':
             ensure  => link,
             target  => "${java_home}/bin/jar",
-            require => Exec["extract_jdk_${version}"],
+            require => Exec["extract_${package}_${version}"],
           }
           file { '/usr/sbin/java':
             ensure  => link,
             target  => '/etc/alternatives/java',
             require => File['/etc/alternatives/java'],
           }
-          file { '/usr/sbin/javac':
-            ensure  => link,
-            target  => '/etc/alternatives/javac',
-            require => File['/etc/alternatives/javac'],
+          if $package != 'jre' {
+            file { '/usr/sbin/javac':
+              ensure  => link,
+              target  => '/etc/alternatives/javac',
+              require => File['/etc/alternatives/javac'],
+            }
           }
           file { '/usr/sbin/jar':
             ensure  => link,
@@ -181,19 +194,19 @@ define jdk_oracle::install(
           file { '/etc/profile.d/java.sh':
             ensure  => present,
             content => "export JAVA_HOME=${java_home}; PATH=\${PATH}:${java_home}/bin",
-            require => Exec["extract_jdk_${version}"],
+            require => Exec["extract_${package}_${version}"],
           }
         }
         if ( $create_symlink ) {
           file { "${install_dir}/java_home":
             ensure  => link,
             target  => $java_home,
-            require => Exec["extract_jdk_${version}"],
+            require => Exec["extract_${package}_${version}"],
           }
-          file { "${install_dir}/jdk-${version}":
+          file { "${install_dir}/${package}-${version}":
             ensure  => link,
             target  => $java_home,
-            require => Exec["extract_jdk_${version}"],
+            require => Exec["extract_${package}_${version}"],
           }
         }
       }
@@ -208,37 +221,45 @@ define jdk_oracle::install(
         if ( $default_java ) {
           # create alternatives configuration for the specified version
           exec { "${path_to_updatealternatives_tool} --install /usr/bin/java java ${java_home}/bin/java 20000":
-            require => Exec["extract_jdk_${version}"],
+            require => Exec["extract_${package}_${version}"],
             unless  => "test $(readlink /etc/alternatives/java) = '${java_home}/bin/java'",
           }
-          exec { "${path_to_updatealternatives_tool} --install /usr/bin/javac javac ${java_home}/bin/javac 20000":
-            require => Exec["extract_jdk_${version}"],
-            unless  => "test $(/bin/readlink /etc/alternatives/javac) = '${java_home}/bin/javac'",
+          if $package != 'jre' {
+            exec { "${path_to_updatealternatives_tool} --install /usr/bin/javac javac ${java_home}/bin/javac 20000":
+              require => Exec["extract_${package}_${version}"],
+              unless  => "test $(/bin/readlink /etc/alternatives/javac) = '${java_home}/bin/javac'",
+            }
           }
           exec { "${path_to_updatealternatives_tool} --install /usr/bin/jar jar ${java_home}/bin/jar 20000":
-            require => Exec["extract_jdk_${version}"],
+            require => Exec["extract_${package}_${version}"],
             unless  => "test $(/bin/readlink /etc/alternatives/jar) = '${java_home}/bin/jar'",
           }
-          exec { "${path_to_updatealternatives_tool} --install /usr/bin/jstack jstack ${java_home}/bin/jstack 20000":
-            require => Exec["extract_jdk_${version}"],
-            unless  => "test $(/bin/readlink /etc/alternatives/jstack) = '${java_home}/bin/jstack'",
+          if $package != 'jre' {
+            exec { "${path_to_updatealternatives_tool} --install /usr/bin/jstack jstack ${java_home}/bin/jstack 20000":
+              require => Exec["extract_${package}_${version}"],
+              unless  => "test $(/bin/readlink /etc/alternatives/jstack) = '${java_home}/bin/jstack'",
+            }
           }
           # activate new alternatives configuration (in case of a version change)
           exec { "${path_to_updatealternatives_tool} --set java ${java_home}/bin/java":
             require => Exec["${path_to_updatealternatives_tool} --install /usr/bin/java java ${java_home}/bin/java 20000"],
             onlyif  => "test $(readlink /etc/alternatives/java) != '${java_home}/bin/java'",
           }
-          exec { "${path_to_updatealternatives_tool} --set javac ${java_home}/bin/javac":
-            require => Exec["${path_to_updatealternatives_tool} --install /usr/bin/javac javac ${java_home}/bin/javac 20000"],
-            onlyif  => "test $(/bin/readlink /etc/alternatives/javac) != '${java_home}/bin/javac'",
+          if $package != 'jre' {
+            exec { "${path_to_updatealternatives_tool} --set javac ${java_home}/bin/javac":
+              require => Exec["${path_to_updatealternatives_tool} --install /usr/bin/javac javac ${java_home}/bin/javac 20000"],
+              onlyif  => "test $(/bin/readlink /etc/alternatives/javac) != '${java_home}/bin/javac'",
+            }
           }
           exec { "${path_to_updatealternatives_tool} --set jar ${java_home}/bin/jar":
             require => Exec["${path_to_updatealternatives_tool} --install /usr/bin/jar jar ${java_home}/bin/jar 20000"],
             onlyif  => "test $(/bin/readlink /etc/alternatives/jar) != '${java_home}/bin/jar'",
           }
-          exec { "${path_to_updatealternatives_tool} --set jstack ${java_home}/bin/jstack":
-            require => Exec["${path_to_updatealternatives_tool} --install /usr/bin/jstack jstack ${java_home}/bin/jstack 20000"],
-            onlyif  => "test $(/bin/readlink /etc/alternatives/jstack) != '${java_home}/bin/jstack'",
+          if $package != 'jre' {
+            exec { "${path_to_updatealternatives_tool} --set jstack ${java_home}/bin/jstack":
+              require => Exec["${path_to_updatealternatives_tool} --install /usr/bin/jstack jstack ${java_home}/bin/jstack 20000"],
+              onlyif  => "test $(/bin/readlink /etc/alternatives/jstack) != '${java_home}/bin/jstack'",
+            }
           }
           augeas { 'environment':
             context => '/files/etc/environment',
