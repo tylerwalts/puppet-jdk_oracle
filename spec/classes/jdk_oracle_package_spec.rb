8d1cfaebe93e15517d446250f80d886da613dd87
Vianney Foucault <vianney.foucault@gmail.com>
2016-10-18 10:58:01 +0200

    Update module to add the a custom URL Feature.
    ```puppet
    class { 'jdk_oracle':
      jce                              => true,
      default                    => true,
      download_url     => 'https://nexus.corp/java',
      proxy_host           => 'http://proxyhost:3128',
      version_update => '102',
      version_build      => '14',
    }
    ```
    
    Refactored the whole module to use
    * puppet-archive to simplify archive download and extraction
    * puppet-alternatives to simplify alternatives setup
    
    Refactored spec tests
    Added Acceptance tests with beaker
    
    Working under debian 8 and centos 6.7 for jdk8/jdk7/jre8 with/without JCE
    
    Should work on major RedHat based release as well as Debian.
    
    ** not tested with suse **

---
+++ spec/classes/jdk_oracle_package_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe 'jdk_oracle' do
+  let(:facts) { {
+        :operatingsystem => 'CentOS',
+        :osfamily        => 'RedHat'
+    }}
+  context 'with default values for all parameters' do
+    it { should contain_jdk_oracle__install('jdk_oracle')}
+    it { is_expected.to contain_Archive('/opt/jdk-8u11-linux-x64.tar.gz')
+         .with_source('http://download.oracle.com/otn-pub/java/jdk/8u11-b12/jdk-8u11-linux-x64.tar.gz')
+         .with_extract_path('/opt/jdk1.8.0_11/..')
+    }
+
+    context 'with jce => true' do
+      let(:params) { {
+          :jce => true
+      }}
+      it { is_expected.to contain_Archive('/opt/jce_policy-8.zip')
+                              .with_source('http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip')
+                              .with_extract_command('unzip -d /opt/jdk1.8.0_11/jre/lib/security -o -j %s')
+      }
+    end
+    context 'with custom download url ' do
+      let(:params) { {
+          :download_url => 'http://onpremise_webhost/java'
+      }}
+      it { is_expected.to contain_Archive('/opt/jdk-8u11-linux-x64.tar.gz')
+                              .with_source('http://onpremise_webhost/java/jdk/8u11-b12/jdk-8u11-linux-x64.tar.gz')
+      }
+    end
+    context 'with proxy_host' do
+      let(:params) { {
+          :proxy_host => 'http://proxy_host:3128'
+      }}
+      it { is_expected.to contain_Archive('/opt/jdk-8u11-linux-x64.tar.gz')
+                              .with_proxy_server('http://proxy_host:3128')
+      }
+    end
+    context 'with custom installation path' do
+      let(:params) { {
+          :install_dir => '/path/to/installation/directory'
+      }}
+      it { is_expected.to contain_Archive('/path/to/installation/directory/jdk-8u11-linux-x64.tar.gz')
+                              .with_extract_path('/path/to/installation/directory/jdk1.8.0_11/..')
+      }
+    end
+    context 'with build and update number' do
+      let(:params) { {
+          :version_update => '102',
+          :version_build => '14'
+      }}
+      it { is_expected.to contain_Archive('/opt/jdk-8u102-linux-x64.tar.gz')
+                              .with_source('http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz')
+                              .with_extract_path('/opt/jdk1.8.0_102/..')
+      }
+    end
+  end
+end
